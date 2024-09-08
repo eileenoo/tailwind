2a5d2d9c2c5760feb5237c9d174bd556ad627904
EileenVerena <verena.schlott@netlight.com>
2024-09-08 15:31:30 +0200

    feat: add style todos to header

--- src/components/Header/SZPlusGroupTitle.tsx
+++ src/components/Header/SZPlusGroupTitle.tsx
@@ -6,14 +6,50 @@ export interface SZPlusGroupTitleProps {
   href: string;
 }
 
+/* 
+  header 
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 8px;
+      margin-bottom: 32px;
+      width: 100%;
+
+  a (wrapper for h2 and icon)
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      border-bottom: solid 1px transparent;
+
+  a:hover 
+    border-color: #29293a;
+
+  h2 (Titel)
+    font-family: ui-serif, Georgia, Cambria, "Times New Roman", Times, serif;
+    font-size: 27px;
+    font-weight: 400;
+    letter-spacing: 0.025em;
+    line-height: 1.3;
+    white-space: normal; 
+    color: #29293a
+    
+  Icon (IoIosArrowForward -> Icon Arrow right)
+    margin-bottom: 1px; 
+    margin-left: 8px;
+*/
+
 export const SZPlusGroupTitle: React.FC<SZPlusGroupTitleProps> = ({
   title,
   href,
 }) => (
-  <header>
-    <a href={href} target="_blank" rel="noopener noreferrer">
-      <h2>{title}</h2>
-      <IoIosArrowForward aria-hidden />
+  // TODO: add styles for header
+  <header className="">
+    {/* TODO: add styles for link (wrapper for h2 and icon) */}
+    <a className="" href={href} target="_blank" rel="noopener noreferrer">
+      {/* TODO: add styles for h2-title, */}
+      <h2 className="">{title}</h2>
+      {/* TODO: add styles for icon */}
+      <IoIosArrowForward className="" aria-hidden />
     </a>
     <SZPlusLogo href={href} />
   </header>
